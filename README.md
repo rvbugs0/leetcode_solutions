61c332a5620764b50e749095564744dea6f0c3e6
Ravi Ailani <rvbugs0@gmail.com>
2022-09-10 17:26:04 -0500

    two sum added

---
+++ README.md
@@ -0,0 +1,6 @@
+# leetcode_solutions
+
+
+
+1. Two sum: https://leetcode.com/problems/two-sum/
+Solution: 
---
+++ two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> sortedArrayWithIndices;
+        //storin 
+        for(int i=0;i<nums.size();i++){
+            sortedArrayWithIndices.push_back([nums[i],i]);
+        }
+        sort(v.begin(),v.end());
+        int i=0;
+        int j=nums.size()-1;
+        
+
+
+        
+    }
+};
